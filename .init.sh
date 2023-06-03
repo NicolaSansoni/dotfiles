4ce36222fc1be7e753e6536b14a4a81ea51159ee
nsanso <nsansoni97@gmail.com>
2023-06-03 18:27:24 +0200

    -y for dnf install

--- .init.sh
+++ .init.sh
@@ -49,7 +49,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 groupadd docker
 usermod -aG docker $LOGNAME
 
