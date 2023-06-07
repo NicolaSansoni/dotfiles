04bfa83f61ee3ca49c506b0dbb44a9d2e5eafd53
nsanso <nsansoni97@gmail.com>
2023-06-07 08:59:10 +0200

    .

--- .config/myinit/config.sh
+++ .config/myinit/config.sh
@@ -3,13 +3,13 @@
 LOGNAME=$(logname)
 
 groupadd -r autologin
-gpasswd -a $LOGNAME autologin
+usermod -aG autologin $LOGNAME
 
 groupadd docker
 usermod -aG docker $LOGNAME
 
-sed -i "s/#autologin-user=/autologin-user=\"$LOGNAME\"/" /etc/lightdm/lightdm.conf
-sed -i "s/#autologin-session=/autologin-session=\"i3\"/" /etc/lightdm/lightdm.conf
+sed -i "s/#autologin-user=/autologin-user=$LOGNAME/" /etc/lightdm/lightdm.conf
+sed -i "s/#autologin-session=/autologin-session=i3/" /etc/lightdm/lightdm.conf
 
 sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee /etc/X11/xorg.conf.d/90-touchpad.conf <<'EOF' 1>/dev/null
 Section "InputClass"
