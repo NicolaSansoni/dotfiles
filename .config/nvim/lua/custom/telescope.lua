dfd7af08c7357ea8d875e5e719bee42ccdc3e93b
nsanso <nsansoni97@gmail.com>
2023-07-15 21:58:36 +0200

    telescope find hidden + small fix? for xrandr race condition

--- .config/nvim/lua/custom/telescope.lua
+++ .config/nvim/lua/custom/telescope.lua
@@ -13,6 +13,11 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('telescope').setup {
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         defaults = {
           mappings = {
             i = {
