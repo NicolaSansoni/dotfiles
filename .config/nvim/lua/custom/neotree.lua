da7eb47db7a84bc19e41f09435ec8be23db836c7
nsanso <nsansoni97@gmail.com>
2023-07-27 13:17:53 +0200

    fix

--- .config/nvim/lua/custom/neotree.lua
+++ .config/nvim/lua/custom/neotree.lua
@@ -46,8 +46,8 @@ return {
         enable_git_status = true,
         enable_diagnostics = true,
         open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
-        sort_case_insensitive = false, -- used when sorting files and directories in the tree
-        sort_function = nil, -- use a custom function for sorting files and directories in the tree
+        sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
+        sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
         -- sort_function = function (a,b)
         --       if a.type == b.type then
         --           return a.path > b.path
@@ -94,7 +94,7 @@ return {
           git_status = {
             symbols = {
               -- Change type
-              added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+              added = '',    -- or "✚", but this is redundant info if you use git_status_colors on the name
               modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
               deleted = '✖', -- this can only be used in the git_status source
               renamed = '', -- this can only be used in the git_status source
@@ -120,7 +120,7 @@ return {
           },
           mappings = {
             ['<2-LeftMouse>'] = 'open',
-            ['<cr>'] = 'toggle_node',
+            ['<cr>'] = 'open',
             ['<esc>'] = 'revert_preview',
             ['S'] = 'open_split',
             ['s'] = 'open_vsplit',
@@ -171,9 +171,9 @@ return {
               --".null-ls_*",
             },
           },
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
+          follow_current_file = true,             -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
-          group_empty_dirs = false, -- when true, empty folders will be grouped together
+          group_empty_dirs = false,               -- when true, empty folders will be grouped together
           hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
           -- in whatever position is specified in window.position
           -- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -193,7 +193,7 @@ return {
         buffers = {
           follow_current_file = true, -- This will find and focus the file in the active buffer every
           -- time the current file is changed while the tree is open.
-          group_empty_dirs = true, -- when true, empty folders will be grouped together
+          group_empty_dirs = true,    -- when true, empty folders will be grouped together
           show_unloaded = true,
           window = {
             mappings = {
