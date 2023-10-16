d760725ad9939ea23e9881a97281adcbb2e229ad
nsanso <nsansoni97@gmail.com>
2023-10-16 15:52:39 +0200

    .

--- .config/myinit/arch/setup.sh
+++ .config/myinit/arch/setup.sh
@@ -23,7 +23,7 @@ yay -Y --devel --save
 yay -Syu
 
 # virualization
-pacman -S --noconfirm qemu-full libvirt virt-manager
+pacman -S --noconfirm dnsmasq qemu-full libvirt virt-manager
 
 systemctl enable libvirtd
 systemctl start libvirtd
--- .config/myinit/arch/tostapane.sh
+++ .config/myinit/arch/tostapane.sh
@@ -18,8 +18,8 @@ tee .xprofile <<'EOF' 1>/dev/null
 
 # X configuration
 xrandr --dpi 192 \
-	--output DP-2 -s 3840x2160 --rate 60.00 \
-	--output HDMI-0 -s 1920x1080 --scale 2x2 --rate 60.00 --primary
+	--output HDMI-0 -s 1920x1080 --scale 2x2 --rate 60.00 --primary \
+	--output DP-2 -s 3840x2160 --rate 60.00 --left-of HDMI-0
 
 xset m 0                                           # mouse accel
 xset b 0                                           # bell
