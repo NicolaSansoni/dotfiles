27db861c5f5a3b5851aa6f1331b3d568cdb45812
nsanso <nsansoni97@gmail.com>
2024-05-11 13:22:44 +0200

    .

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,15 +1,24 @@
 return {
-    "numToStr/Comment.nvim",
-    dependencies = {
-        "JoosepAlviste/nvim-ts-context-commentstring",
+    -- {
+    --     "numToStr/Comment.nvim",
+    --     dependencies = {
+    --         "JoosepAlviste/nvim-ts-context-commentstring",
+    --     },
+    --     opts = {},
+    --     keys = {
+    --         { "gc", mode = { "n", "v" } },
+    --         { "gb", mode = { "n", "v" } },
+    --     },
+    --     config = function(_, opts)
+    --         opts.pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+    --         require("Comment").setup(opts)
+    --     end,
+    -- },
+    {
+        "echasnovski/mini.comment",
+        keys = {
+            "gc",
+        },
+        opts = {},
     },
-    opts = {},
-    keys = {
-        { "gc", mode = { "n", "v" } },
-        { "gb", mode = { "n", "v" } },
-    },
-    config = function(_, opts)
-        opts.pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-        require("Comment").setup(opts)
-    end,
 }
